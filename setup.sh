2eede88ea2c04cf3fed6d206016beba403a844d2
Andrea Picciau <a.picciau13@imperial.ac.uk>
2015-10-05 17:32:13 +0100

    Add a setup file for Linux/UNIX

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is the setup script for dotfiles.
+# Must be run from the directory 'dotfiles'
+
+set -e
+
+if [[ ! -f vimrc ]]; then
+	echo "Run this script from the 'dotfiles' directory"
+	exit -1
+fi
+
+ln -sf $(pwd)/vimrc 	~/.vimrc
+ln -sf $(pwd)/gnuplot 	~/.gnuplot
