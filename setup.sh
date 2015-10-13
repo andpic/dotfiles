e6224230b34396d41c63d516ec51c4839d87e60c
Andrea Picciau <a.picciau13@imperial.ac.uk>
2015-10-13 16:24:44 +0100

    Change the setup to discriminate between ubuntu and other distros.

--- setup.sh
+++ setup.sh
@@ -12,5 +12,9 @@ fi
 
 ln -sf $(pwd)/vimrc 		~/.vimrc
 ln -sf $(pwd)/gnuplot 		~/.gnuplot
-ln -sf $(pwd)/vim.desktop 	~/.local/share/applications/vim.desktop
 ln -sf $(pwd)/screenrc 		~/.screenrc
+
+# Changes specific to Ubuntu
+if grep 'ubuntu' /proc/version; then 
+	ln -sf $(pwd)/vim.desktop 	~/.local/share/applications/vim.desktop
+fi
